7a83d634c186dbdbe175439af5f782aeacdc24b2
Wasir <wasir@koer.no>
2024-12-27 18:01:05 +0600

    project created

---
+++ iOSMessanger/Core/Authentication/View/LoginView.swift
@@ -0,0 +1,22 @@
+//
+//  LoginView.swift
+//  iOSMessanger
+//
+//  Created by Wasir on 27/12/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        NavigationStack{
+            VStack {
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    LoginView()
+}
