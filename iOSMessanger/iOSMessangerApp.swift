fb996f0d087998a5bcb5c90feb7e1b42bd836f4c
Wasir <wasir@koer.no>
2024-12-27 17:54:05 +0600

    Initial Commit

---
+++ iOSMessanger/iOSMessangerApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSMessangerApp.swift
+//  iOSMessanger
+//
+//  Created by Wasir on 27/12/24.
+//
+
+import SwiftUI
+
+@main
+struct iOSMessangerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
